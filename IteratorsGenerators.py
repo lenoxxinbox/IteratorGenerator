0a536ec52ad04255e7db155ba720acc2160cf131
lenoxxinbox <lenoxxinbox150279@gmail.com>
2021-12-02 22:28:20 +0300

    Initial commit

---
+++ IteratorsGenerators.py
@@ -0,0 +1,51 @@
+class FlatIterator:
+    def __init__(self, root):
+        self.out_start = 0
+        self.out_end = len(root)
+        self.in_start = 0
+        self.in_end = None
+        self.step = 1
+        self.root = root
+        self.out_cur = None
+        self.inner_cur = None
+
+    def __iter__(self):
+        self.out_cur = self.root[self.out_start]
+        self.in_end = len(self.out_cur)
+        return self
+
+    def __next__(self):
+        if self.in_start >= self.in_end:
+            self.out_start += self.step
+            if self.out_start >= self.out_end:
+                raise StopIteration
+            self.out_cur = self.root[self.out_start]
+            self.in_end = len(self.out_cur)
+            self.in_start = 0
+
+        self.inner_cur = self.out_cur[self.in_start]
+        self.in_start += self.step
+
+        return self.inner_cur
+
+
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    [1, 2, None]
+]
+
+for item in FlatIterator(nested_list):
+    print(item)
+
+print('*' * 100)
+
+
+def flat_generator(nested_list):
+    for n in nested_list:
+        for a in n:
+            yield a
+
+
+for item in flat_generator(nested_list):
+    print(item)
